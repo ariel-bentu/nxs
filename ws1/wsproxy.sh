9a024d1cb2629ad28632c3bd576bf4c2364a3534
Ariel Bentolila <ariel.bentolila@sap.com>
2018-09-07 12:26:31 +0300

    .

--- ws1/wsproxy.sh
+++ ws1/wsproxy.sh
@@ -7,13 +7,13 @@ mkdir -p ${CHE_DIR}
 mkdir -p ${WS_PROXY_DIR}
 
 #TODO verify version
-if [ -f  $WS_PROXY_EXE ]; 
-    then; 
-    else; 
-        echo "Files are not yet install, installing..."; 
-        curl https://github.com/ariel-bentu/nxs/blob/master/ws1/bin/ws_proxy?raw=true --output $WS_PROXY_EXE
-        chmod +x 
-fi;
+if [ -f  ${WS_PROXY_EXE} ]; then 
+        echo "Files are up to date."
+    else 
+        echo "Files are not yet install, installing..."
+        curl -L https://github.com/ariel-bentu/nxs/raw/master/ws1/bin/ws_proxy --output $WS_PROXY_EXE
+        chmod +x ${WS_PROXY_EXE}
+fi
 
 echo '[Upstream]\nURL=<TO BE REPLACED>\n\n[Cookie]\n`<TO BE REPLACED>`\n' > $WS_PROXY_DIR/proxy.ini
 $WS_PROXY_EXE -f ./proxy.ini run
